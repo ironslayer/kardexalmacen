39707b8af4987cbc04adf7e6d62ce872ad26d64a
Alvaro Guarachi <alvaro.guarachi.ticona@gmail.com>
2023-10-10 21:22:36 -0400

    modal dereingreso de registros de unidades

--- app/Views/unidades/eliminados.php
+++ app/Views/unidades/eliminados.php
@@ -30,9 +30,9 @@
                                 <td> <?php echo $dato['id_unidadmedida']; ?> </td>
                                 <td> <?php echo $dato['nombre_unidad']; ?> </td>
 
-                                <td> <a href="<?php echo base_url().'unidades/reingresar/'.$dato['id_unidadmedida']; ?>" class="btn btn-primary"><i class="fas fa-arrow-alt-circle-up"></i></a></td>
 
 
+                                <td> <a href="#" data-href="<?php echo base_url().'unidades/reingresar/'.$dato['id_unidadmedida']; ?>" data-bs-toggle="modal" data-bs-target="#modalConfirma" title="Reingresar registro" class="btn btn-primary"><i class="fas fa-arrow-alt-circle-up"></i></a></td>
 
 
 
@@ -46,3 +46,24 @@
             </div>
         </div>
     </main>
+
+    <!-- modal -->
+    <div class="modal fade" id="modalConfirma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Reingresar registro</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>¿Desea reingresar este registro?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <a class="btn btn-primary btn-ok">Si</a>
+            </div>
+            </div>
+        </div>
+    </div>
